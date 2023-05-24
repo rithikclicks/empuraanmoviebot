0ad10cd1b3aa9f822328b3ef5fd87b45bdf870cc
rithikclicks <134219084+rithikclicks@users.noreply.github.com>
2023-05-24 16:07:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,15 +19,15 @@ BOT_TOKEN = environ['6207887069:AAFtxEz3VEe8SiZdawIpjdXDWs43HE0Pwmo']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/8b42f6caf6ef5fd76766f.jpg https://telegra.ph/file/82b5bbbab6d5e5593b6b2.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/Ws2z0PM/images-20.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6019216032').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-ADMINS.append(1684438752)
-auth_channel = environ.get('AUTH_CHANNEL')
+ADMINS.append(6019216032)
+auth_channel = environ.get('-1001754886821')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
